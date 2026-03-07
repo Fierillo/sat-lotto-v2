29de8f4f8de122da4813505c3e1180b6f2aa86de
fierillo <fierillo@protonmail.com>
2026-03-07 12:23:10 -0300

    feat: use localStorage to eliminate block height flicker on load

--- src/components/state.ts
+++ src/components/state.ts
@@ -1,7 +1,9 @@
 export const BLOCKS: number = 21;
 
+const savedBlock = JSON.parse(localStorage.getItem('satlotto_blocks') || '{}');
+
 export const state = {
-    currentBlock: 892341,
-    targetBlock: 892341 + BLOCKS - 1,
+    currentBlock: savedBlock.height || 890000,
+    targetBlock: savedBlock.target || (890000 + BLOCKS),
     selectedNumber: null as number | null
 };
