2429ebe09fa78362bec79340859c467a77f26d7b
fierillo <fierillo@protonmail.com>
2026-03-09 14:27:47 -0300

    perf: trigger early UI update on login to avoid async delays

--- src/components/auth-manager.ts
+++ src/components/auth-manager.ts
@@ -330,6 +330,9 @@ export async function finishLogin(): Promise<void> {
         if (authState.nwcUrl) localStorage.setItem('satlotto_nwc', authState.nwcUrl);
         if (authState.bunkerTarget) localStorage.setItem('satlotto_bunker', authState.bunkerTarget);
 
+        // Update UI immediately if we have a pubkey
+        updateAuthUI();
+
         const withTimeout = (promise: Promise<any>, ms: number, msg: string) => 
             Promise.race([promise, new Promise((_, reject) => setTimeout(() => reject(new Error(msg)), ms))]);
 
