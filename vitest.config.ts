870dc4b173eade0f085f3367fdc2f4a992120e38
fierillo <fierillo@protonmail.com>
2026-03-21 15:15:36 -0300

    chore: add vitest testing framework

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['tests/unit/**/*.ts', 'tests/integration/**/*.ts', 'tests/security/**/*.ts'],
+    exclude: ['tests/debug/**', '**/node_modules/**'],
+    environment: 'node',
+    globals: true,
+  },
+});
